38ac13edb8a7f130cccb46697d536d64e979773b
Denis Babuskin <3169477@gmail.com>
2018-10-05 15:55:15 +0300

    Counter modification

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 module.exports = function longestConsecutiveLength(array) {
-  //compare numbers
-  function compareNum(a, b) {
-    return a - b;
-  }
-  //one value
-  if (array.length == 1) {
-    return 1;
-  }
   //empty array
   if (array.length == 0) {
     return 0;
   }
   //return array in ascending order
-  let ar = array.sort(compareNum);
-  
+  let ar = array.sort((a, b) => a - b);
   let sum = 0, max = 0;
   for (let i = 0; i < ar.length-1; i++) {
     if (Math.abs(ar[i]-ar[i+1]) == 1) {
